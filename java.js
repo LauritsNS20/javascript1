df7b4d38929cbc00172b1d2080fe0d6b72446546
rts-6905 <U2418040@rts-365.dk>
2023-08-28 12:21:58 +0200

    movies

---
+++ java.js
@@ -0,0 +1,64 @@
+ 
+
+var myMovies = [
+    {
+        title: "Forest Gump",
+        punlished: 1994,
+        genres: ["drama"],
+        actors: ["Tom Hanks"],
+    },
+    {
+        title: "John Wick",
+        punlished: 2017,
+        genres: ["action"],
+        actors: ["keanu reeves", "Laurence Fishburne"],
+    },
+    {
+        title: "Alien",
+        punlished: 1979,
+        genres: ["horror"],
+        actors: ["Sigourney Weaver", "Bolaji Badejo"],
+    },
+    {
+        title: "Lord of the rings",
+        punlished: 2001,
+        genres: ["fantasy"],
+        actors: ["Elijah Wood", "Viggo Mortensen"],
+    },
+    {
+        title: "batman dark knigth rises",
+        punlished: 2012,
+        genres: ["action"],
+        actors: ["Christian Bale", "Joseph Gordon-Levitt"],
+    },
+    {
+        title: "Guardians of the Galaxy 3",
+        punlished: 2023,
+        genres: ["action", "fantasy"],
+        actors: ["Chris Pratt", "Will Poulter"],
+    },
+    {
+        title: "Star Wars Episode I",
+        punlished: 1999,
+        genres: ["sci-fi", "action"],
+        actors: ["Ewan McGregor", "Liam Neeson"],
+    },
+    {
+        title: "The Matrix",
+        punlished: 1999,
+        genres: ["action", "sci-fi"],
+        actors: ["Keanu Reeves", "Laurence Fishburne", "Hugo Weaving"],
+    },
+    {
+        title: "Top Gun",
+        punlished: 1986,
+        genres: ["action", "romance"],
+        actors: ["Val Kilmer", "Tom Cruise"],
+    },
+    {
+        title: "Predator",
+        punlished: 1987,
+        genres: ["horror", "action"],
+        actors: ["Arnold Schwarzenegger", "Kevin Peter Hall"],
+    },
+]
